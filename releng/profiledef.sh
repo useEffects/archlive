35ce3a3b4feaf3b21866cf7ad455e04a7a3d1663
april <joel.sr1024@gmail.com>
2023-08-29 21:11:18 +0530

    changes

--- releng/profiledef.sh
+++ releng/profiledef.sh
@@ -24,4 +24,10 @@ file_permissions=(
   ["/usr/local/bin/choose-mirror"]="0:0:755"
   ["/usr/local/bin/Installation_guide"]="0:0:755"
   ["/usr/local/bin/livecd-sound"]="0:0:755"
+  ["/etc/polkit-1/rules.d"]="0:0:750"
+  ["/etc/sudoers.d"]="0:0:750"
+  ["/usr/local/bin/alci-make-a-pure-arch"]="0:0:755"
+  ["/usr/local/bin/alci-displaymanager-check"]="0:0:755"
+  ["/usr/local/bin/get-nemesis-on-alci"]="0:0:755"
+  ["/usr/local/bin/fix-keys"]="0:0:755"
 )
